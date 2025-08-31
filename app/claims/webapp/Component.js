24f7e244a23126adf7cc0a39e9afe165960ce7ca
Hoang <hoang.duong@hotmail.de>
2025-08-31 16:18:40 +0200

    fix(ui): initialize chat button from Component.init to ensure load within FE app lifecycle

--- app/claims/webapp/Component.js
+++ app/claims/webapp/Component.js
@@ -1,12 +1,24 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "de/claimpilot/claims/ext/chatInit"
+], function (AppComponent, chatInit) {
+    "use strict";
 
-        return Component.extend("de.claimpilot.claims.Component", {
-            metadata: {
-                manifest: "json"
+    return AppComponent.extend("de.claimpilot.claims.Component", {
+        metadata: {
+            manifest: "json"
+        },
+        init: function () {
+            AppComponent.prototype.init.apply(this, arguments);
+            try {
+                if (chatInit && typeof chatInit.init === 'function') {
+                    chatInit.init();
+                }
+            } catch (e) {
+                // fail-safe: ignore UI init errors
+                /* eslint-disable no-console */
+                console && console.warn && console.warn("Chat init failed", e);
             }
-        });
-    }
-);
+        }
+    });
+});
